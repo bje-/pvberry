dc7e2ebe16844cbe8bcc616f1ea5b4bfe872839d
Ben Elliston <bje@air.net.au>
2016-07-03 21:52:19 +1000

    Remove PIN_SAVING_HARDWARE ifdefs.

--- pvberry.ino
+++ pvberry.ino
@@ -14,11 +14,6 @@
  * CT1 is for 'grid' current, to be measured at the grid supply point.
  * CT2 is for the load current, so that diverted energy can be recorded
  *
- * A persistence-based 4-digit display is supported. This can be driven in two
- * different ways, one with an extra pair of logic chips, and one without.  The 
- * appropriate version of the sketch must be selected by including or commenting 
- * out the "#define PIN_SAVING_HARDWARE" statement near the top of the code.
- *
  * September 2014: renamed as Mk2_bothDisplays_2, with these changes:
  * - cycleCount removed (was not actually used in this sketch, but could have overflowed);
  * - removal of unhelpful comments in the IO pin section;
@@ -72,12 +67,6 @@
 // to prevent the diverted energy total from 'creeping'
 #define ANTI_CREEP_LIMIT 5 // in Joules per mains cycle (has no effect when set to 0)
 long antiCreepLimit_inIEUperMainsCycle;
-
-//  The two versions of the hardware require different logic.  The following line should 
-//  be included if the additional logic chips are present, or excluded if they are 
-//  absent (in which case some wire links need to be fitted)
-//
-#define PIN_SAVING_HARDWARE 
 
 // definition of enumerated types
 enum polarities {NEGATIVE, POSITIVE};
@@ -244,63 +233,6 @@ const byte noOfPossibleCharacters = 22;
 #define DISPLAY_SHUTDOWN_IN_HOURS 8 // auto-reset after this period of inactivity
 // #define DISPLAY_SHUTDOWN_IN_HOURS 0.01 // for testing that the display clears after 36 seconds
 
-//  The two versions of the hardware require different logic.
-#ifdef PIN_SAVING_HARDWARE
-
-#define DRIVER_CHIP_DISABLED HIGH
-#define DRIVER_CHIP_ENABLED LOW
-
-// the primary segments are controlled by a pair of logic chips
-const byte noOfDigitSelectionLines = 4; // <- for the 74HC4543 7-segment display driver
-const byte noOfDigitLocationLines = 2; // <- for the 74HC138 2->4 line demultiplexer
-
-byte enableDisableLine = 5; // <- affects the primary 7 segments only (not the DP)
-byte decimalPointLine = 14; // <- this line has to be individually controlled. 
-
-byte digitLocationLine[noOfDigitLocationLines] = {16,15};
-byte digitSelectionLine[noOfDigitSelectionLines] = {7,9,8,6};
-
-// The final column of digitValueMap[] is for the decimal point status.  In this version, 
-// the decimal point has to be treated differently than the other seven segments, so 
-// a convenient means of accessing this column is provided.
-//
-byte digitValueMap[noOfPossibleCharacters][noOfDigitSelectionLines +1] = {
-                 LOW , LOW , LOW , LOW , LOW , // '0' <- element 0
-                 LOW , LOW , LOW , HIGH, LOW , // '1' <- element 1
-                 LOW , LOW , HIGH, LOW , LOW , // '2' <- element 2
-                 LOW , LOW , HIGH, HIGH, LOW , // '3' <- element 3
-                 LOW , HIGH, LOW , LOW , LOW , // '4' <- element 4
-                 LOW , HIGH, LOW , HIGH, LOW , // '5' <- element 5
-                 LOW , HIGH, HIGH, LOW , LOW , // '6' <- element 6
-                 LOW , HIGH, HIGH, HIGH, LOW , // '7' <- element 7
-                 HIGH, LOW , LOW , LOW , LOW , // '8' <- element 8
-                 HIGH, LOW , LOW , HIGH, LOW , // '9' <- element 9
-                 LOW , LOW , LOW , LOW , HIGH, // '0.' <- element 10
-                 LOW , LOW , LOW , HIGH, HIGH, // '1.' <- element 11
-                 LOW , LOW , HIGH, LOW , HIGH, // '2.' <- element 12
-                 LOW , LOW , HIGH, HIGH, HIGH, // '3.' <- element 13
-                 LOW , HIGH, LOW , LOW , HIGH, // '4.' <- element 14
-                 LOW , HIGH, LOW , HIGH, HIGH, // '5.' <- element 15
-                 LOW , HIGH, HIGH, LOW , HIGH, // '6.' <- element 16
-                 LOW , HIGH, HIGH, HIGH, HIGH, // '7.' <- element 17
-                 HIGH, LOW , LOW , LOW , HIGH, // '8.' <- element 18
-                 HIGH, LOW , LOW , HIGH, HIGH, // '9.' <- element 19
-                 HIGH, HIGH, HIGH, HIGH, LOW , // ' '  <- element 20          
-                 HIGH, HIGH, HIGH, HIGH, HIGH  // '.'  <- element 21
-};
-
-// a tidy means of identifying the DP status data when accessing the above table
-const byte DPstatus_columnID = noOfDigitSelectionLines; 
-
-byte digitLocationMap[noOfDigitLocations][noOfDigitLocationLines] = {
-                 LOW , LOW ,    // Digit 1
-                 LOW , HIGH,    // Digit 2
-                 HIGH, LOW ,    // Digit 3
-                 HIGH, HIGH,    // Digit 4
-};
-
-#else // PIN_SAVING_HARDWARE
-
 #define ON HIGH
 #define OFF LOW
 
@@ -338,7 +270,6 @@ byte segMap[noOfPossibleCharacters][noOfSegmentsPerDigit] = {
                  OFF, OFF, OFF, OFF, OFF, OFF, OFF, OFF, // ' ' <- element 20
                  OFF, OFF, OFF, OFF, OFF, OFF, OFF, ON  // '.' <- element 11
 };
-#endif // PIN_SAVING_HARDWARE
 
 byte charsForDisplay[noOfDigitLocations] = {20,20,20,20}; // all blank 
 
@@ -365,24 +296,6 @@ void setup()
   Serial.println("Sketch ID:      Mk2_bothDisplays_4.ino");
   Serial.println();
        
-#ifdef PIN_SAVING_HARDWARE
-  // configure the IO drivers for the 4-digit display   
-  //
-  // the Decimal Point line is driven directly from the processor
-  pinMode(decimalPointLine, OUTPUT); // the 'decimal point' line 
-  
-  // set up the control lines for the 74HC4543 7-seg display driver
-  for (int i = 0; i < noOfDigitSelectionLines; i++) {
-    pinMode(digitSelectionLine[i], OUTPUT); }
-
-  // an enable line is required for the 74HC4543 7-seg display driver
-  pinMode(enableDisableLine, OUTPUT); // for the 74HC4543 7-seg display driver
-  digitalWrite( enableDisableLine, DRIVER_CHIP_DISABLED);  
-  
-  // set up the control lines for the 74HC138 2->4 demux
-  for (int i = 0; i < noOfDigitLocationLines; i++) {
-    pinMode(digitLocationLine[i], OUTPUT); } 
-#else
   for (int i = 0; i < noOfSegmentsPerDigit; i++) {
     pinMode(segmentDrivePin[i], OUTPUT); }
   
@@ -394,7 +307,6 @@ void setup()
   
   for (int i = 0; i < noOfSegmentsPerDigit; i++) {
     digitalWrite(segmentDrivePin[i], OFF); }
-#endif
       
   
        
@@ -1041,66 +953,6 @@ void refreshDisplay()
   // the next digit.
   //   The two versions of the hardware require different logic.
  
-#ifdef PIN_SAVING_HARDWARE
-  // With this version of the hardware, care must be taken that all transitory states
-  // are masked out.  Note that the enableDisableLine only masks the seven primary
-  // segments, not the Decimal Point line which must therefore be treated separately.
-  // The sequence is:
-  // 
-  // 1. set the decimal point line to 'off'
-  // 2. disable the 7-segment driver chip 
-  // 3. determine the next location which is to be active
-  // 4. set up the location lines for the new active location
-  // 5. determine the relevant character for the new active location
-  // 6. configure the driver chip for the new character to be displayed
-  // 7. set up decimal point line for the new active location
-  // 8. enable the 7-segment driver chip 
-  
-  static byte displayTime_count = 0;
-  static byte digitLocationThatIsActive = 0;
-  
-  displayTime_count++;
-  
-  if (displayTime_count > MAX_DISPLAY_TIME_COUNT)
-  {
-    byte lineState;
-    
-    displayTime_count = 0;   
-
-    // 1. disable the Decimal Point driver line;
-      digitalWrite( decimalPointLine, LOW); 
-        
-    // 2. disable the driver chip while changes are taking place
-    digitalWrite( enableDisableLine, DRIVER_CHIP_DISABLED);  
-
-    // 3. determine the next digit location to be active
-    digitLocationThatIsActive++;
-    if (digitLocationThatIsActive >= noOfDigitLocations) {
-      digitLocationThatIsActive = 0; }
-           
-    // 4. set up the digit location drivers for the new active location
-    for (byte line = 0; line < noOfDigitLocationLines; line++) {
-      lineState = digitLocationMap[digitLocationThatIsActive][line];
-      digitalWrite( digitLocationLine[line], lineState); }
-        
-    // 5. determine the character to be displayed at this new location
-    // (which includes the decimal point information)
-    byte digitVal = charsForDisplay[digitLocationThatIsActive];
-    
-    // 6. configure the 7-segment driver for the character to be displayed 
-    for (byte line = 0; line < noOfDigitSelectionLines; line++) { 
-      lineState = digitValueMap[digitVal][line];
-      digitalWrite( digitSelectionLine[line], lineState); }
-      
-    // 7. set up the Decimal Point driver line;
-      digitalWrite( decimalPointLine, digitValueMap[digitVal][DPstatus_columnID]); 
-      
-    // 8. enable the 7-segment driver chip   
-    digitalWrite( enableDisableLine, DRIVER_CHIP_ENABLED);  
-  }
-
-#else // PIN_SAVING_HARDWARE
-
   // This version is more straightforward because the digit-enable lines can be
   // used to mask out all of the transitory states, including the Decimal Point.
   // The sequence is:
@@ -1139,7 +991,6 @@ void refreshDisplay()
   // 5. activate the digit-enable line for the new active location 
     digitalWrite(digitSelectorPin[digitLocationThatIsActive], DIGIT_ENABLED);   
   } 
-#endif // PIN_SAVING_HARDWARE
 
 } // end of refreshDisplay()
 
