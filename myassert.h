a05b231ca360cb03a2874a3aa6432f9386bfd9c2
Ben Elliston <bje@air.net.au>
2016-07-19 14:32:20 +1000

    * pvberry.ino: Add license notices.

--- myassert.h
+++ myassert.h
@@ -1,3 +1,17 @@
+// myassert.h: Handy assert function for the Arduino.
+//
+// Copyright 2016 Ben Elliston
+//
+// PVBerry is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+
+// PVBerry is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+
 #define __ASSERT_USE_STDERR
 
 #include <assert.h>
