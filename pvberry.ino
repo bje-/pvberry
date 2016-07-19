cc9f79b7236e7e625c6ca9d928b8d8cd603dee18
Ben Elliston <bje@air.net.au>
2016-07-19 14:33:18 +1000

    * pvberry.ino: Improve comments and formatting.

--- pvberry.ino
+++ pvberry.ino
@@ -14,28 +14,29 @@
 #include <avr/wdt.h>
 #include <Arduino.h>
 #include <TimerOne.h>
-#include <myassert.h>
+
+#include "myassert.h"
 
 // uS (determines the sampling rate / amount of idle time)
 const int ADC_TIMER_PERIOD = 125;
 
 // Physical constants, please do not change!
 const int SECONDS_PER_HOUR = 3600;
-const int JOULES_PER_WATT_HOUR = 3600;   //  (0.001 kWh = 3600 Joules)
+const int JOULES_PER_WATT_HOUR = 3600;
 
 // Change these values to suit the local mains frequency and supply meter
 const int CYCLES_PER_SECOND = 50;
 const int WORKING_RANGE_IN_JOULES = 3600;
 
 enum polarities {NEGATIVE, POSITIVE};
-enum loadStates {LOAD_ON, LOAD_OFF}; // the external trigger device is active low
+enum loadStates {LOAD_ON, LOAD_OFF}; // the triac is active low
 
 void configureParamsForSelectedOutputMode();
 void timerIsr(void);
 void allGeneralProcessing();
 void confirmPolarity();
 
-const byte outputForTrigger = 4;       // <-- an output which is active-low
+const byte outputForTrigger = 4;
 const byte voltageSensor = 3;          // A3 is for the voltage sensor
 const byte currentSensor = 4;          // A4 is for CT measuring grid current
 
@@ -91,11 +92,12 @@ void setup()
 #endif
 
     pinMode(outputForTrigger, OUTPUT);
-    digitalWrite(outputForTrigger, LOAD_OFF); // the external trigger is active low
+    digitalWrite(outputForTrigger, LOAD_OFF);
 
-    delay(delayBeforeSerialStarts * 1000); // allow time to open Serial monitor
+    // allow time to open Serial monitor
+    delay(delayBeforeSerialStarts * 1000);
     Serial.begin(9600);
-    Serial.println("pvberry.ino");
+    Serial.println("Welcome to the PVBerry!");
     Serial.println();
 
     // When using integer maths, calibration values that have been
@@ -125,19 +127,13 @@ void setup()
     DCoffset_V_max = (long)(512L + 100) * 256; // mid-point of ADC plus a working margin
 
     // Set up the ADC to be triggered by a hardware timer of fixed duration
-    ADCSRA  = (1<<ADPS0)+(1<<ADPS1)+(1<<ADPS2);  // Set the ADC's clock to system clock / 128
-    ADCSRA |= (1 << ADEN);                 // Enable ADC
+    // Set the ADC's clock to system clock / 128
+    ADCSRA  = (1 << ADPS0) + (1 << ADPS1) + (1 << ADPS2);
+    // Enable ADC
+    ADCSRA |= (1 << ADEN);
 
-    Timer1.initialize(ADC_TIMER_PERIOD);   // set Timer1 interval
-    Timer1.attachInterrupt( timerIsr );    // declare timerIsr() as interrupt service routine
-
-    Serial.println();
-    Serial.print("powerCal = " + String(powerCal, 4));
-
-    Serial.print("zero-crossing persistence (sample sets) = ");
-    Serial.println(PERSISTENCE_FOR_POLARITY_CHANGE);
-    Serial.print("continuity sampling display rate (mains cycles) = ");
-    Serial.println(CONTINUITY_CHECK_MAXCOUNT);
+    Timer1.initialize(ADC_TIMER_PERIOD);
+    Timer1.attachInterrupt(timerIsr);
 
     lowerEnergyThreshold_long =
 	capacityOfEnergyBucket_long * (0.5 - offsetOfEnergyThresholdsInAFmode);
@@ -155,7 +151,7 @@ void timerIsr(void)
     case 0:
         sampleV = ADC;                    // store the voltage
         ADMUX = 0x40 + currentSensor;     // switch ADC channel
-        ADCSRA |= (1<<ADSC);              // start a conversion
+        ADCSRA |= (1 << ADSC);            // start a conversion
         state = 1;
         sampleI = sampleI_raw;
         dataReady = true;                 // both ADC values can now be processed
@@ -163,7 +159,7 @@ void timerIsr(void)
     case 1:
         sampleI_raw = ADC;                // store the current
         ADMUX = 0x40 + voltageSensor;	  // switch ADC channel
-        ADCSRA |= (1<<ADSC);              // start a conversion
+        ADCSRA |= (1 << ADSC);            // start a conversion
         state = 0;
         break;
     default:
@@ -193,7 +189,7 @@ void allGeneralProcessing()
 
     // remove DC offset from the raw voltage sample by subtracting the accurate value
     // as determined by a LP filter.
-    long sampleVminusDC_long = ((long) sampleV<<8) - DCoffset_V_long;
+    long sampleVminusDC_long = ((long) sampleV << 8) - DCoffset_V_long;
 
     // determine the polarity of the latest voltage sample
     polarityOfMostRecentVsample = (sampleVminusDC_long > 0) ? POSITIVE : NEGATIVE;
@@ -207,47 +203,16 @@ void allGeneralProcessing()
                 if (sampleSetsDuringThisMainsCycle < lowestNoOfSampleSetsPerMainsCycle)
                     lowestNoOfSampleSetsPerMainsCycle = sampleSetsDuringThisMainsCycle;
 
-                // Calculate the real power and energy during the last whole mains cycle.
-                //
-                // sumP contains the sum of many individual calculations of instantaneous power.  In
-                // order to obtain the average power during the relevant period, sumP must first be
-                // divided by the number of samples that have contributed to its value.
-                //
-                // The next stage would normally be to apply a calibration factor so that real power
-                // can be expressed in Watts.  That's fine for floating point maths, but it's not such
-                // a good idea when integer maths is being used.  To keep the numbers large, and also
-                // to save time, calibration of power is omitted at this stage.  Real Power (stored as
-                // a 'long') is therefore (1/powerCal) times larger than the actual power in Watts.
-
                 long realPower = sumP / sampleSetsDuringThisMainsCycle; // proportional to Watts
 
-                // Next, the energy content of this power rating needs to be determined.  Energy is
-                // power multiplied by time, so the next step is normally to multiply the measured
-                // value of power by the time over which it was measured.
-                //   Instanstaneous power is calculated once every mains cycle. When integer maths is
-                // being used, a repetitive power-to-energy conversion seems an unnecessary workload.
-                // As all sampling periods are of similar duration, it is more efficient simply to
-                // add all of the power samples together, and note that their sum is actually
-                // CYCLES_PER_SECOND greater than it would otherwise be.
-                //   Although the numerical value itself does not change, I thought that a new name
-                // may be helpful so as to minimise confusion.
-
-                // The 'realEnergy' variable below is CYCLES_PER_SECOND * (1/powerCal) times larger
-		// than the actual energy in Joules.
+                // realEnergy is CYCLES_PER_SECOND * (1/powerCal) times larger than
+                // the actual energy in J.
                 long realEnergy = realPower;
 
-                // Energy contributions from the grid connection point (CT1) are summed in an
-                // accumulator which is known as the energy bucket.  The purpose of the energy bucket
-                // is to mimic the operation of the supply meter.  The range over which energy can
-                // pass to and fro without loss or charge to the user is known as its 'sweet-zone'.
-                // The capacity of the energy bucket is set to this same value within setup().
-                //
-                // The latest contribution can now be added to this energy bucket
                 energyInBucket_long += realEnergy;
 
                 // Apply max and min limits to bucket's level.  This is to ensure correct operation
                 // when conditions change, i.e. when import changes to export, and vice-versa.
-                //
                 if (energyInBucket_long > capacityOfEnergyBucket_long)
                     energyInBucket_long = capacityOfEnergyBucket_long;
                 else if (energyInBucket_long < 0)
@@ -257,7 +222,7 @@ void allGeneralProcessing()
                 sampleCount_forContinuityChecker++;
                 if (sampleCount_forContinuityChecker >= CONTINUITY_CHECK_MAXCOUNT) {
                     sampleCount_forContinuityChecker = 0;
-                    Serial.println(lowestNoOfSampleSetsPerMainsCycle);
+		    // Serial.println(lowestNoOfSampleSetsPerMainsCycle);
                     lowestNoOfSampleSetsPerMainsCycle = 999;
                 }
 
@@ -274,7 +239,7 @@ void allGeneralProcessing()
                     sampleSetsDuringThisMainsCycle = 0; // not yet dealt with for this cycle
                     sampleCount_forContinuityChecker = 1; // opportunity has been missed for this cycle
                     lowestNoOfSampleSetsPerMainsCycle = 999;
-                    Serial.println ("Go!");
+                    Serial.println ("Ready.");
                 }
             }
         } // end of processing that is specific to the first Vsample in each +ve half cycle
@@ -284,15 +249,15 @@ void allGeneralProcessing()
         if (sampleSetsDuringThisMainsCycle == 3) { // much easier than checking the voltage level
             if (beyondStartUpPhase) {
                 if (energyInBucket_long < lowerEnergyThreshold_long) {
-                    // when below the lower threshold, always set the load to "off"
+                    // when below the lower threshold, turn the load OFF
                     nextStateOfLoad = LOAD_OFF;
                 } else if (energyInBucket_long > upperEnergyThreshold_long) {
-                    // when above the upper threshold, always set the load to "off"
+                    // when above the upper threshold, turn the load ON
                     nextStateOfLoad = LOAD_ON;
                 }
                 // (else) otherwise, leave the load's state unchanged (hysteresis)
 
-                // set the Arduino's output pin accordingly, and clear the flag
+                // set the Arduino's output pin accordingly
                 digitalWrite(outputForTrigger, nextStateOfLoad);
             }
         }
@@ -306,7 +271,7 @@ void allGeneralProcessing()
             // The portion which is fed back into the integrator is approximately one percent
             // of the average offset of all the Vsamples in the previous mains cycle.
             long previousOffset = DCoffset_V_long;
-            DCoffset_V_long = previousOffset + (cumVdeltasThisCycle_long>>12);
+            DCoffset_V_long = previousOffset + (cumVdeltasThisCycle_long >> 12);
             cumVdeltasThisCycle_long = 0;
 
             // To ensure that the LPF will always start up correctly
@@ -335,8 +300,8 @@ void allGeneralProcessing()
     long filtV_div4 = phaseShiftedSampleVminusDC >> 2;  // reduce to 16-bits (now x64, or 2^6)
     long filtI_div4 = sampleIminusDC >> 2; // reduce to 16-bits (now x64, or 2^6)
     long instP = filtV_div4 * filtI_div4;  // 32-bits (now x4096, or 2^12)
-    instP = instP>>12;     // scaling is now x1, as for Mk2 (V_ADC x I_ADC)
-    sumP += instP; // cumulative power, scaling as for Mk2 (V_ADC x I_ADC)
+    instP = instP >> 12;                   // scaling is now x1, as for Mk2 (V_ADC x I_ADC)
+    sumP += instP;                         // cumulative power, scaling as for Mk2 (V_ADC x I_ADC)
 
     sampleSetsDuringThisMainsCycle++;
 
